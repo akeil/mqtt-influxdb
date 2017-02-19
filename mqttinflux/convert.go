87b3ce2aa0bb7f3a12873afa5031c8622f0a9847
akeil <alex@akeil.net>
2017-02-19 17:16:26 +0100

    conversion supports translation

--- mqttinflux/convert.go
+++ mqttinflux/convert.go
@@ -4,6 +4,7 @@ import (
     "errors"
     "fmt"
     "strconv"
+    "strings"
 )
 
 type Converter func(raw string, params *Conversion) (string, error)
@@ -15,14 +16,26 @@ func init() {
     converters["identity"] = Identity
     converters["float"] = Float
     converters["integer"] = Integer
+    converters["boolean"] = Boolean
+    converters["on-off"] = OnOff
 }
 
 type Conversion struct {
     Kind string `json:"kind"`
     Precision int `json:"precision"`
+    Lookup map[string]string `json:"lookup"`
 }
 
 func (c *Conversion) Convert(raw string) (string, error) {
+    var err error
+
+    if c.Lookup != nil {
+        raw, err = c.translate(raw)
+    }
+    if err != nil {
+        return "", err
+    }
+
     key := c.Kind
     if key == "" {
         key = "identity"
@@ -32,6 +45,15 @@ func (c *Conversion) Convert(raw string) (string, error) {
         return "", errors.New("Conversion not supported")
     }
     return conv(raw, c)
+}
+
+func (c *Conversion) translate(raw string) (string, error) {
+    key := strings.TrimSpace(raw)
+    translated, found := c.Lookup[key]
+    if !found {
+        return "", errors.New("Lookup failed for " + key)
+    }
+    return translated, nil
 }
 
 func Identity(raw string, params *Conversion) (string, error) {
@@ -73,3 +95,27 @@ func Floor(raw string, params *Conversion) (string, error) {
 
 }
 */
+
+func Boolean(raw string, params *Conversion) (string, error) {
+    parsed, err := strconv.ParseBool(raw)
+    if err != nil {
+        return "", err
+    }
+    return fmt.Sprintf("%t", parsed), nil
+}
+
+// converts the string "on" or "off" to a boolean value
+// on=true, off=false
+// case-insensitive
+func OnOff(raw string, params *Conversion) (string, error) {
+    s := strings.TrimSpace(strings.ToLower(raw))
+    var value bool
+    if s == "on" {
+        value = true
+    } else if s == "off" {
+        value = false
+    } else {
+        return "", errors.New("Expected on/off, got " + raw)
+    }
+    return fmt.Sprintf("%t", value), nil
+}
